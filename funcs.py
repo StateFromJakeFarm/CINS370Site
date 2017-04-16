b6f6b12261cd8d964edc8fb7d64bd08568eaf4b5
Ubuntu <ubuntu@ip-172-31-28-65.us-west-2.compute.internal>
2017-04-16 05:12:21 +0000

    basic query function and connect

--- funcs.py
+++ funcs.py
@@ -1,4 +1,10 @@
-import MySQLdb
+import cx_Oracle
 
-def query(queryString):
-    pass
+def connect(user, passwd, host, path):
+	return cx_Oracle.connect(user + '/' + passwd + '@' + host + path)
+
+def query(conHandle, queryString):
+	cur = conHandle.cursor()
+	cur.execute(queryString)
+
+	return cur
--- main.py
+++ main.py
@@ -7,7 +7,19 @@ app = Flask(__name__, static_url_path='')
 
 @app.route('/', methods=['GET', 'POST'])
 def root():
-    return app.send_static_file('index.html')
+	conHandle = connect('user101', 'pass101', '127.0.0.1', '')
+	queryStr = """
+		SELECT * FROM WCPLAYERS14
+	"""
+
+	outStr = ""
+
+	allRows = query(conHandle, queryStr)
+	for row in allRows:
+		outStr += str(row)
+		outStr += '<br>'
+
+	return outStr
 
 if __name__ == '__main__':
     app.run()
