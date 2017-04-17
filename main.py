5877378b2867f38e91c79383b1d24083b366712f
Jake Hansen <jhansen44@mail.csuchico.edu>
2017-04-16 22:04:11 -0700

    start tables page

--- main.py
+++ main.py
@@ -4,19 +4,34 @@ import os
 
 from funcs import *
 
+# DB login credentials, host, and path
+creds = ('user101','pass101','127.0.0.1','/')
+
 app = Flask(__name__, static_url_path='')
 
 class QueryForm(Form):
     query = TextField('Query', [validators.Required()])
 
 @app.route('/query', methods=['GET', 'POST'])
-def anyQuery():
-    form = QueryForm()
-    if request.method == 'POST':
-        return request.form['query']
+def query():
+    # DB login credentials, host, and path
+    creds = ('user101','pass101','127.0.0.1','/')
 
+    # Form to hold our query
+    form = QueryForm()
+
+    # If we get a POST request (the query), display output in table
+    if request.method == 'POST':
+        queryStr = request.form['query']
+        dataRows = query(creds, queryStr)
+        return makeTable("", dataRows)
+
+    # Else, display our template
     return render_template('query.html', form=form)
 
+@app.route('/tables', methods=['GET', 'POST'])
+def tables():
+    return ""
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
